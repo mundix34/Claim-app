501f8066395afc751507d37c5f05db4eb7e13297
mundix34 <mundix34@gmail.com>
2018-08-16 09:18:21 -0600

    edit partial

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 import { addFirstName, addLastName, addEmail, addClaim, addAddressOne, addAddressTwo, addCity, addReference, isInsured, addState, addZip, addProfile, clearFields, addUserInfo } from '../../ducks/reducer';
 import axios from 'axios';
 import styled from 'styled-components';
-import './Input.css';
+import './Edit.css';
+import { Col, Row } from 'react-bootstrap';
+
 
 
 
@@ -17,22 +19,7 @@ margin-top: 20px;
 height: 100%;
 
 `
-const FormDiv = styled.div`
-box-shadow: 0 0 20px 0 rgba(72, 94, 116, 0.7);
-padding: 1.5rem;
-border-radius: 0.5rem;
-margin: 1em;
-height: 80%;
 
-
-`
-
-const formStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-    alignItems: 'flex start',
-}
 const MapDiv = styled.div`
 color: ddd;
 display: flex;
@@ -56,28 +43,8 @@ margin: 1rem;
 //       background:#efb247;
 //   }
 `
-const pStyle = {
-    marginRight: '1rem',
 
 
-}
-const SelectDiv = styled.div`
-display: flex;
-justify-content: space-evenly;
-margin: 1.4rem auto;
-`
-const InputField = styled.input`
-border-radius: 5px;
-margin: 1.5rem;
-box-shadow: none;
-border: 1px solid grey;
-flexBasis: 4px;
-`
-const ButtonDiv = styled.div`
-display: flex;
-justify-content: center;
-
-`
 const P = styled.p`
 font-weight: 600;
 `
@@ -87,7 +54,7 @@ const listStyle = {
 }
 
 
-class Input extends Component {
+class Edit extends Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -98,62 +65,7 @@ class Input extends Component {
             form: true
 
         }
-        this.nextPage = this.nextPage.bind(this);
-        this.showEdit = this.showEdit.bind(this);
-        this.addProfile = this.addProfile.bind(this);
         this.editProfile = this.editProfile.bind(this);
-    }
-    // componentDidMount() {
-    //     axios.get('/api/user_data').then(res => {            
-    //         this.props.addUserInfo(res.data)
-    //     })
-    // }
-    // componentDidUpdate() {
-    //     axios.get('/api/user_data').then(res => {
-    //         this.props.addUserInfo(res.data)
-    //     })
-    // }
-
-    addProfile() {
-        if (!this.props.newUser.firstName) {
-            alert('FirstName is required')
-        } else if (!this.props.newUser.lastName) {
-            alert('Last Name is required')
-        } else if (!this.props.newUser.addressOne) {
-            alert('Address is required')
-        }  else if (!this.props.newUser.city) {
-            alert('City is required')
-        } else if (!this.props.newUser.state) {
-            alert('State is required')
-        } else if (!this.props.newUser.zip) {
-            alert('Zip Code is required')
-        }
-        else if (!this.props.newUser.claim) {
-            alert('Claim Number is required')
-        }
-        else if (!this.props.newUser.reference) {
-            alert('Reference Number is required')
-        }
-        else {
-            axios.post(`/api/register/${this.props.user.user_id}`, this.props.newUser).then(res => {
-            console.log(res.data.response);
-                this.props.addProfile(res.data.response)
-                this.setState({
-                    ref_id: res.data.response.ref_id,
-                    stateUser: [res.data.response],
-                    form: false
-                
-
-                })
-            })
-        }
-    }
-
-    showEdit() {
-        this.setState({
-            form: true,
-            edit: true
-        })
     }
 
 
@@ -168,11 +80,7 @@ class Input extends Component {
             })
         })
     }
-    nextPage() {
-        this.props.newUser.insured === "yes" ? this.props.history.push(`/dashboard/${this.state.ref_id}`) :
-            this.props.newUser.insured === "no" ? this.props.history.push(`/summary/${this.state.ref_id}`) :
-                alert('please make a selection')
-    }
+
     render() {
         const newStateUser = this.state.stateUser.map((item, i) => (
             <div style={listStyle} className=" animated bounceInRight" key={i}>
@@ -205,62 +113,6 @@ class Input extends Component {
             <Outer>
                 <Row>
                     <Col xs="6">
-                        <FormDiv className={this.state.form ? "animated slideInLeft" : "not-showing animated slideInLeft"}>
-                            <form style={formStyle} onSubmit={e => e.preventDefault()}>
-                                <label> Fields marked with an asterisk (*) are Required</label>
-                                <span>{this.props.newUser.firstName}</span>
-                                <label>First Name  </label><InputField className="input" type="text" placeholder="First Name   * " value={this.props.newUser.firstName} onChange={(e) => this.props.addFirstName(e.target.value.toUpperCase())} />
-
-                                <span>{this.props.newUser.lastName}</span>
-                                <label>Last Name </label><InputField className="input" type="text" placeholder="Last Name   * " value={this.props.newUser.lastName} onChange={(e) => this.props.addLastName(e.target.value.toUpperCase())} />
-
-                                <span>{this.props.newUser.email}</span>
-                                <label>Email </label><InputField className="input" type="email" placeholder="Email   * " value={this.props.newUser.email} onChange={(e) => this.props.addEmail(e.target.value)} />
-
-                                <span>{this.props.newUser.addressOne}</span>
-                                <label>Address 1  </label><InputField className="input" type="text" placeholder="Address Line 1   * " value={this.props.newUser.addressOne} onChange={(e) => this.props.addAddressOne(e.target.value.toUpperCase())} />
-
-                                <span>{this.props.newUser.addressTwo}</span>
-                                <label>Address 2 (Optional) </label><InputField type="text" className="input" placeholder="Address Line 2" value={this.props.newUser.addressTwo} onChange={(e) => this.props.addAddressTwo(e.target.value.toUpperCase())} />
-                                <span>{this.props.newUser.city}</span>
-
-                                <label>City </label><InputField className="input" type="text" placeholder="City  *" value={this.props.newUser.city} onChange={(e) => this.props.addCity(e.target.value.toUpperCase())} />
-                                <span>{this.props.newUser.state}</span>
-
-                                <label>State </label> <InputField className="input" type="text" placeholder="State *" value={this.props.newUser.state} onChange={(e) => this.props.addState(e.target.value.toUpperCase())} />
-                                <span>{this.props.newUser.zip}</span>
-
-                                <label>Zip Code </label> <InputField type="number" className="input" value={this.props.newUser.zip} placeholder="Zip Code *" onChange={(e) => this.props.addZip(e.target.value)} />
-                                <span>{this.props.newUser.reference}</span>
-
-                                <label>Claim Number </label> <InputField type="number" className="input" value={this.props.newUser.claim} placeholder="Claim Number *" onChange={(e) => this.props.addClaim(e.target.value)} />
-                                <span>{this.props.newUser.claim}</span>
-
-
-                                <label>Reference ID ?</label> <InputField className="input" type="number" value={this.props.newUser.reference} placeholder="Reference ID *" onChange={(e) => this.props.addReference(e.target.value)} />
-                                <SelectDiv>
-                                    <label style={pStyle}> Are you insured with Insurance Inc?</label>
-                                    <select className="select" onChange={(e) => this.props.isInsured(e.target.value)}>
-                                        <option type="text" value="select" >select</option>
-                                        <option type="text" value="yes" >Yes</option>
-                                        <option type="text" value="no" >No</option>
-                                    </select>
-                                </SelectDiv>
-                                <ButtonDiv>
-
-                                    <Button className="hov" onClick={() => this.props.clearFields()}>Cancel</Button>
-                                    {!this.state.edit ? <Button type="submit" onClick={this.addProfile}>Submit</Button> :
-                                        <Button className="hov" type="submit" onClick={this.editProfile}>Save</Button>}
-
-                                </ButtonDiv>
-
-
-
-                            </form>
-                        </FormDiv>
-                    </Col>
-
-                    <Col xs="6">
                         <MapDiv>{newStateUser}</MapDiv>
                     </Col>
 
@@ -292,4 +144,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, { addFirstName, addLastName, addEmail, addClaim, addAddressOne, addAddressTwo, addCity, addReference, isInsured, addState, addZip, addProfile, clearFields, addUserInfo })(Input)
+export default connect(mapStateToProps, { addFirstName, addLastName, addEmail, addClaim, addAddressOne, addAddressTwo, addCity, addReference, isInsured, addState, addZip, addProfile, clearFields, addUserInfo })(Edit)
